74fe04cb1d3854d8bc415d8a1f5fb9e2e2f10c8c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-09 07:49:44 -0500

    fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 // === UserData Root Url ===//
 
 export const customFetch = axios.create({
-  baseURL: 'https://inamecommerce.herokuapp.com/api/v1',
+  baseURL: 'https://aryanaspa.herokuapp.com/api/v1',
 })
